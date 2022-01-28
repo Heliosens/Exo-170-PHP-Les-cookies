caef617b7caa870f4d5454d2b655664288617bc2
Sylvie-B <unsolo@hotmail.fr>
2022-01-28 09:43:24 +0100

    cookie

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="bio.php">vers bio</a>
+</body>
+</html>
 <?php
 
 /**
@@ -5,8 +18,8 @@
  * 2. Affichez la valeur de ce cookie avec un simple echo.
  */
 // TODO Votre code ici.
-
-
+//setcookie('username', 'Sylvie');
+//echo $_COOKIE['username'];
 
 /**
  * 3. Commentez le code du point 1 et 2
@@ -14,3 +27,5 @@
  * 5. Créez une page bio.php et tentez d'afficher le cookie username sur cette page.
  */
 // TODO Votre code ici.
+setcookie('username', 'Sylvie', time() + 3600 * 50, '/', 'localhost');
+
